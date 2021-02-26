268a4f82412090e066148ce1b78a69966464db82
nksxn <vibhu.saxena31@gmail.com>
2021-02-26 23:56:55 +0530

    Update ProviderDaoImpl.java
    added provider dummy data to list

--- providers/src/main/java/com/nagarro/nagp/providers/impl/ProviderDaoImpl.java
+++ providers/src/main/java/com/nagarro/nagp/providers/impl/ProviderDaoImpl.java
@@ -1,8 +1,12 @@
 package com.nagarro.nagp.providers.impl;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +21,38 @@ public class ProviderDaoImpl implements ProviderDao {
 	private static final Logger LOG = LoggerFactory.getLogger(ProviderDaoImpl.class);
 
 	private static List<Provider> providers = new ArrayList<Provider>();
+
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+	@PostConstruct
+	public void init() {
+		LOG.info("Creting Dummy data for Users.");
+		LocalDateTime now = LocalDateTime.now();
+		Provider provider1 = new Provider();
+		provider1.setActive(Boolean.TRUE);
+		provider1.setFirstName("Vinay");
+		provider1.setLastName("Prajapati");
+		provider1.setEmail("vinay.prajapati@gmail.com");
+		provider1.setPhone("9813232319");
+		provider1.setUsername("vinayPrajapati");
+		provider1.setPassword("qwerty@123");
+		provider1.setExpertise("Carpainter");
+		provider1.setCreationTime(dtf.format(now));
+		provider1.setModifiedTime(dtf.format(now));
+		Provider provider2 = new Provider();
+		provider2.setActive(Boolean.TRUE);
+		provider2.setFirstName("Gaurav");
+		provider2.setLastName("Kansal");
+		provider2.setEmail("gaurav.kansal@gmail.com");
+		provider2.setPhone("9827632981");
+		provider2.setUsername("gauravKansal");
+		provider2.setPassword("qwerty@123");
+		provider2.setExpertise("Painter");
+		provider2.setCreationTime(dtf.format(now));
+		provider2.setModifiedTime(dtf.format(now));
+		providers.add(provider1);
+		providers.add(provider2);
+	}
 
 	@Override
 	public List<Provider> getAllProviders() {

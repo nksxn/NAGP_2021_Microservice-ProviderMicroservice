1164fc2268a4ebb454d21a57867291e2be7d4857
nksxn <vibhu.saxena31@gmail.com>
2021-02-26 23:33:15 +0530

    Update ProviderController.java
    Controller created

--- providers/src/main/java/com/nagarro/nagp/providers/controller/ProviderController.java
+++ providers/src/main/java/com/nagarro/nagp/providers/controller/ProviderController.java
@@ -10,30 +10,65 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nagarro.nagp.providers.entity.Provider;
 import com.nagarro.nagp.providers.facade.ProviderFacade;
 
 @RestController
 @RequestMapping(value = "/providers")
 public class ProviderController {
 	private static final Logger LOG = LoggerFactory.getLogger(ProviderController.class);
-	
+
 	@Value("${server.port}")
 	private int port;
-	
+
 	@Autowired
 	ProviderFacade providerFacade;
-	
+
 	@GetMapping
 	public ResponseEntity<Map<String, Object>> getAllProviders() {
-		
-		LOG.info("Working from port " + port +" of Providers microservice");
-		
+
+		LOG.info("Working from port " + port + " of Providers microservice");
+
 		Map<String, Object> responseData = providerFacade.getAllProviders();
 
 		return new ResponseEntity<Map<String, Object>>(responseData, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	@PostMapping()
+	public ResponseEntity<Void> addProvider(@RequestBody Provider provider) {
+		LOG.info("Working from port " + port + " of Providers microservice");
+		try {
+			providerFacade.addProvider(provider);
+			HttpHeaders headers = new HttpHeaders();
+			return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+		} catch (Exception e) {
+			LOG.error(e.getMessage());
+			return new ResponseEntity<Void>(new HttpHeaders(), HttpStatus.SERVICE_UNAVAILABLE);
+		}
+	}
+
+	@GetMapping("/{username}")
+	public ResponseEntity<Map<String, Object>> getProviderByUsername(@PathVariable("username") String username) {
+		LOG.info("Working from port " + port + " of Users microservice");
+		Map<String, Object> responseData = providerFacade.getProviderByUsername(username);
+		return new ResponseEntity<Map<String, Object>>(responseData, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	@PutMapping()
+	public ResponseEntity<Void> updateProvider(@RequestBody Provider provider) {
+		LOG.info("Working from port " + port + " of Users microservice");
+		HttpHeaders headers = new HttpHeaders();
+		if (providerFacade.updateProvider(provider)) {
+			return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+		}
+		return new ResponseEntity<Void>(headers, HttpStatus.BAD_REQUEST);
 	}
 
 }
